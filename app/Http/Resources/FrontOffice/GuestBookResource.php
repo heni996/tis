3280f31bdb67599160484a07d2c3433a2649e570
heni996 <henizeiri19@gmail.com>
2024-02-10 21:42:34 +0100

    fix

--- app/Http/Resources/FrontOffice/GuestBookResource.php
+++ app/Http/Resources/FrontOffice/GuestBookResource.php
@@ -16,17 +16,17 @@ class GuestBookResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'client_first_name' => $this->client_first_name,
-            'client_last_name' => $this->client_last_name,
-            'email' => $this->email,
-            'phone_number' => $this->phone_number,
-            'extra_comment' => $this->extra_comment,
-            'hotel_id' => $this->hotel_id,
-            'language' => $this->language,
-            'country' => $this->country,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-            'hotel' => new HotelResource($this->whenLoaded('hotel')),
+            // 'client_first_name' => $this->client_first_name,
+            // 'client_last_name' => $this->client_last_name,
+            // 'email' => $this->email,
+            // 'phone_number' => $this->phone_number,
+            // 'extra_comment' => $this->extra_comment,
+            // 'hotel_id' => $this->hotel_id,
+            // 'language' => $this->language,
+            // 'country' => $this->country,
+            // 'created_at' => $this->created_at,
+            // 'updated_at' => $this->updated_at,
+            // 'hotel' => new HotelResource($this->whenLoaded('hotel')),
         ];
     }
 }
